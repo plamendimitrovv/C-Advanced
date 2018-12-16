96c36a99ca0ed82821862ff75b22665800d3b048
plamendimitrovv <45126707+plamendimitrovv@users.noreply.github.com>
2018-12-16 17:57:05 +0100

    Add files via upload

---
+++ Stacks and Queues/1. Reverse Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var stack = new Stack<char>();
+            foreach (var character in input)
+            {
+                stack.Push(character);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop().ToString());
+            }
+        }
+    }
+}
---
+++ Stacks and Queues/2. Simple Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var elements = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var stack = new Stack<string>();
+
+            for (int i = elements.Length - 1; i >= 0; i--)
+            {
+                stack.Push(elements[i]);
+            }
+
+            while (stack.Count > 1)
+            {
+                var leftOperand = int.Parse(stack.Pop());
+                var operation = stack.Pop();
+                var rightOperand = int.Parse(stack.Pop());
+                switch (operation)
+                {
+                    case "+":
+                        stack.Push((leftOperand + rightOperand).ToString());
+                        break;
+                    case "-":
+                        stack.Push((leftOperand - rightOperand).ToString());
+                        break;
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
